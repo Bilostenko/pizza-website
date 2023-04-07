76ba73091e664a3a32cf6ebada4be856ee906c7d
Bilostenko <bilostenkod@gmail.com>
2023-04-07 22:03:26 +0300

    add constructor JS

--- main.js
+++ main.js
@@ -9,3 +9,97 @@ window.addEventListener('scroll', function () {
       stickyNav.classList.remove('fixed');
   }
 });
+
+// pizza consturcotr
+/* drag and drop */
+const dragItems = document.querySelectorAll('.drag-item');
+const dropContainer = document.querySelector('.drop-container');
+
+dragItems.forEach(item => {
+  item.addEventListener('dragstart', dragStart);
+  item.addEventListener('dragend', dragEnd);
+});
+
+dropContainer.addEventListener('dragover', dragOver);
+dropContainer.addEventListener('dragenter', dragEnter);
+dropContainer.addEventListener('dragleave', dragLeave);
+dropContainer.addEventListener('drop', drop);
+
+let draggedItem = null;
+
+function dragStart() {
+  draggedItem = this;
+  setTimeout(() => {
+    this.style.opacity = '0.6';
+  }, 0);
+}
+
+function dragEnd() {
+  draggedItem = null;
+  this.style.opacity = '1';
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  this.classList.add('hovered');
+}
+
+function dragLeave() {
+  this.classList.remove('hovered');
+}
+
+function drop() {
+  const input = draggedItem.querySelector('input[type="checkbox"]');
+  input.checked = true;
+  this.classList.remove('hovered');
+  updateTotalPrice();
+}
+
+
+// price calculation
+const totalPrice = document.querySelector('.total__price');
+const sizeControls = document.querySelector('.size-controls');
+const inputs = sizeControls.querySelectorAll('input');
+
+/* корж */
+inputs.forEach(input => {
+  input.addEventListener('change', () => {
+    const checkedInput = sizeControls.querySelector('input:checked');
+
+    if (checkedInput) {
+      updateTotalPrice();
+    }
+  });
+});
+
+/* ingradients */
+const ingredientInputs = document.querySelectorAll('.ingredient-input');
+
+ingredientInputs.forEach(input => {
+  input.addEventListener('change', () => {
+    updateTotalPrice();
+  });
+});
+
+function updateTotalPrice() {
+  let totalPriceValue = 0;
+
+  /* Calculation of the cost of selected ingredients */
+  ingredientInputs.forEach(input => {
+    if (input.checked) {
+      totalPriceValue += parseFloat(input.value);
+    }
+  });
+
+  /* Adding the cost of the selected size */
+  const checkedInput = sizeControls.querySelector('input:checked');
+  if (checkedInput) {
+    totalPriceValue += parseFloat(checkedInput.value);
+  }
+
+  totalPrice.innerHTML = "Total price: " + totalPriceValue + "$";
+}
