b77076fb33eed09f14af3de170b319dd6a793f3c
Bilostenko <bilostenkod@gmail.com>
2023-04-09 00:04:27 +0300

    add combo pizza

---
+++ combo.mjs
@@ -0,0 +1,22 @@
+const combo = [
+  {
+    "image": "https://veteranopizza.com/public/uploads/site_catalog/original_5845686cf3b5fd46a15e3811380a7391.png",
+    "name": "Комбо S",
+    "description": "Піца Маргарита + Піца Мисливська + Піца Гавайська + Піца Вегетаріанська",  
+    "cost": "199",
+  },
+  {
+    "image": "https://veteranopizza.com/public/uploads/site_catalog/original_d861077e37de281e368c49db2e02042d.png",
+    "name": "Комбо M",
+    "description": "Піца Песто Прошуто 30см. + Прованс 30см.+ Українська 30см. + Coca-Cola 0.5л. 3шт",
+    "cost": "299",
+  },
+  {
+    "image": "https://veteranopizza.com/public/uploads/site_catalog/original_32504bb98ead1fba947c0350c9f11596.jpg",
+    "name": "Комбо L",
+    "description": "Піца Песто Прошуто 30см. + Прованс 30см.+ Українська 30см. + Coca-Cola 0.5л. 3шт",
+    "cost": "399",
+  }
+]
+
+export default combo
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import pizza from './pizza.mjs';
+import combo from './combo.mjs';
 
 // sticky navbar
 window.addEventListener('scroll', function () {
@@ -106,6 +107,23 @@ function updateTotalPrice() {
   totalPrice.innerHTML = "До сплати: " + totalPriceValue + "$";
 }
 
+// statistics
+
+// отримати елементи DOM для чисел піци та клієнтів
+const pizzaCount = document.querySelector('.statistic__pizza');
+const clientCount = document.querySelector('.statistic__clients');
+
+// оновлюємо число піци кожні 3 секунди
+setInterval(() => {
+  pizzaCount.textContent = (parseFloat(pizzaCount.textContent) + 1).toFixed(0);
+}, 3000);
+
+// оновлюємо число клієнтів кожні 5 секунд
+setInterval(() => {
+  clientCount.textContent = (parseFloat(clientCount.textContent) + 1).toFixed(0);
+}, 5000);
+
+
 // cards container
 const cardsContainer = document.getElementById("cards-container");
 
@@ -119,9 +137,9 @@ pizza.forEach((card) => {
   const cardDescription = document.createElement("p");
   cardDescription.innerText = card.description;
   const cardcost = document.createElement("p");
-  cardcost.innerText = "Ціна: " + card.cost;
+  cardcost.innerText = "Ціна: " + card.cost + "$";
   const cardButton = document.createElement("button");
-  cardButton.innerText = "Замовити";
+  cardButton.innerText = "В кошик";
 
   cardDiv.appendChild(cardImg);
   cardDiv.appendChild(cardName);
@@ -129,4 +147,33 @@ pizza.forEach((card) => {
   cardDiv.appendChild(cardcost);
   cardDiv.appendChild(cardButton);
   cardsContainer.appendChild(cardDiv);
+});
+
+// combo
+const comboPizza = document.getElementById("combo-pizza");
+
+combo.forEach((card) => {
+  const cardDiv = document.createElement("div");
+  cardDiv.classList.add("card");
+  const cardImg = document.createElement("img");
+  cardImg.src = card.image;
+  const cardName = document.createElement("p");
+  cardName.innerText = card.name;
+  const cardTitle = document.createElement("p");
+  cardTitle.innerText = "Комбо меню";
+  cardTitle.classList.add("combo-title");
+  const cardDescription = document.createElement("p");
+  cardDescription.innerText = card.description;
+  const cardcost = document.createElement("p");
+  cardcost.innerText = "Ціна: " + card.cost + "$";
+  const cardButton = document.createElement("button");
+  cardButton.innerText = "В кошик";
+
+  cardDiv.appendChild(cardTitle);
+  cardDiv.appendChild(cardImg);
+  cardDiv.appendChild(cardName);
+  cardDiv.appendChild(cardDescription);
+  cardDiv.appendChild(cardcost);
+  cardDiv.appendChild(cardButton);
+  comboPizza.appendChild(cardDiv);
 });
