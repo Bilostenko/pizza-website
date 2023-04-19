c9b5c7c612f01f051104a1659f9879634a83fcfc
Bilostenko <bilostenkod@gmail.com>
2023-04-19 17:55:42 +0300

    add js

--- pizza.mjs
+++ pizza.mjs
@@ -4,13 +4,13 @@ const pizza = [
     "description": "Вершкова основа, моцарелла, рікота, груша, горгондзола, грецький горіх",
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_f8e5d58e48dd0b15a2d15737925e4e56.jpg",
     "cost": "10$",
-    "data-i18n": "bla-bla-bla"
+    "data-i18n": "dolce-pere"
   },
   {
     "name": "Тендітний Помідор",
     "description": "Вершкова основа, моцарелла, рікота, помідор, помідори черрі, пармезан",
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_d3c8726c47d80b7800a03216342be5f1.png",
-    "data-i18n": "ololololo"
+    "data-i18n": "tendet-tomato"
   },
   {
     "name": "Песто Прошуто",
