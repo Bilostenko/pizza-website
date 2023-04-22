80b01f8cf8725188c69f18dfc522a60def901244
Bilostenko <bilostenkod@gmail.com>
2023-04-23 00:09:20 +0300

    add database filling

--- pizza.mjs
+++ pizza.mjs
@@ -1,132 +1,132 @@
 const pizza = [
   {
-    "pizza-name": "pere",
+    "pizza-name": "dolce-pere-name",
     "description": "Вершкова основа, моцарелла, рікота, груша, горгондзола, грецький горіх",
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_f8e5d58e48dd0b15a2d15737925e4e56.jpg",
     "cost": "10",
-    "data-i18n": "dolce-pere"    
+    "data-i18n": "dolce-pere"
   },
   {
-    "pizza-name": "tendet_tomato",
+    "pizza-name": "tendet_tomato-name",
     "description": "Вершкова основа, моцарелла, рікота, помідор, помідори черрі, пармезан",
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_d3c8726c47d80b7800a03216342be5f1.png", "cost": "10",
     "data-i18n": "tendet-tomato",
 
   },
   {
-    "name": "Песто Прошуто",
+    "pizza-name": "pesto-proshuto-name",
     "description": "Томатна основа, моцарелла, шинка, помідори черрі, соус Песто",
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_89c9d02a6808b42b28224a4110420eb3.jpg",
     "cost": "7",
     "data-i18n": "pesto-proshuto"
   },
   {
-    "name": "Tenerezza",
+    "pizza-name": "tenerezza-name",
     "description": "Вершкова основа, моцарелла, рікота, італійське м'ясо Di Parma, рукола, пармезан",
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_5d530bec9f3167eb91f585e3e92cf95d.jpg",
     "cost": "11",
     "data-i18n": "tenerezza"
   },
   {
-    "name": "Пепероні",
+      "pizza-name": "peperoni-name",
     "description": "Томатна основа, моцарела, салямі pepperoni",
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_31df4042c5b3719a616a8ad40751e854.jpg",
     "cost": "6",
     "data-i18n": "peperoni"
   },
   {
-    "name": "Diablo",
+     "pizza-name": "diablo-name",
     "description": "Томатна основа, моцарела, салямі Picanto",
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_15f5a6329654e1ce4ea27ecef1620d0a.jpg",
     "cost": "24",
     "data-i18n": "diablo"
   },
   {
-    "name": "Salmon",
+    "pizza-name": "salmon-name",
     "description": "Вершкова основа, моцарела, лосось, пармезан, рiкота, рукола, червона iкра.",
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_7d20f493a26b73ed49c9226e6f705e34.jpg",
     "cost": "12",
     "data-i18n": "salmon"
   },
   {
-    "name": "Цезар",
+    "pizza-name": "caesar-name",
     "description": "Вершкова основа,лист салату,моцарела,соус Цезар, курка, пармезан, помідори черрі",
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_c22bff41004968c5603343b3642cea1d.jpg",
     "cost": "12",
     "data-i18n": "caesar"
   },
   {
-    "name": "Піца Капрезе",
+    "pizza-name": "caprese-name",
     "description": "Вершки, моцарела, моцарела буффало, томати чері, в‘ялені томати, соус песто",
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_1a3f7390dfe50e2f508dd69e6f397633.JPG",
     "cost": "13",
     "data-i18n": "caprese"
   },
   {
-    "name": "Піца Американська",
+    "pizza-name": "americano-name",
     "description": "Італійські томати, соус BBQ, моцарела, мисливські ковбаски, курка, гірчиця американська",
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_409b140cf91887bbd07dabe8ad614549.JPG",
     "cost": "11",
     "data-i18n": "americano"
   },
   {
-    "name": "М'ясна 2.0",
+    "pizza-name": "meat-2.0-name",
     "description": "Томатна основа, моцарела, салямі Пепероні, шинка, бекон",
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_34fd35d473dacd53e30882e98fa2e477.jpg",
     "cost": "14",
     "data-i18n": "meat-2.0"
   },
   {
-    "name": "Квадро Стаджоні",
+    "pizza-name": "quattro-stagioni-name",
     "description": "Томатна основа, моцарела, помідори, печериці, пепероні, шинка, пармезан.",
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_094babb1b2a781bcc51210a4f35b0217.jpg",
     "cost": "4",
     "data-i18n": "quattro-stagioni"
   },
   {
-    "name": "Тірольська",
+    "pizza-name": "tirol-name",
     "description": "Томатна основа, моцарела, мисливські ковбаски, салямі, корнішони, перець гострий",
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_7e7b0a49ff059449dcd10a4bd58f25e5.jpg",
     "cost": "15",
     "data-i18n": "tirol"
   },
   {
-    "name": "Примавера",
+    "pizza-name": "primavera-name",
     "description": "Вершкова основа, моцарела, цукіні, куряче філе, чері, часник",
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_545107c12c1e740ac1c01e07d43e3d5f.jpg",
     "cost": "21",
     "data-i18n": "primavera"
   },
   {
-    "name": "Капричоза",
+    "pizza-name": "kaprychosa-name",
     "description": "Томатна основа, моцарела, шинка, печериці, артишок, часник",
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_651d748d82dc6ac0ff78fd0a5f6d03c7.jpg",
     "cost": "10",
     "data-i18n": "kaprychosa"
   },
   {
-    "name": "З білими грибами",
+    "pizza-name": "white-mashroom-name",
     "description": "Вершкова основа, моцарела, горгонзола, білі гриби",
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_cd5cbbf4b0de873c2631533c531a0255.jpg",
     "cost": "11",
     "data-i18n": "white-mashroom"
   },
   {
-    "name": "Одуванчік",
+    "pizza-name": "dandelion-name",
     "description": "Вершкова основа, моцарела, салямі, бекон, шинка, курка, печериці, цибуля",
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_17782bf41c8212593194888e75f6af22.jpg",
     "cost": "16",
     "data-i18n": "dandelion"
   },
   {
-    "name": "Українська",
+    "pizza-name": "ukrainian-name",
     "description": "Томатна основа, моцарела, бекон, шинка, салямi, помiдори, цибуля",
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_644c57c6479bee31db4e589488933a00.jpg",
     "cost": "17",
     "data-i18n": "ukrainian"
   },
   {
-    "name": "Для Буслаєва",
+    "pizza-name": "for-buslaeva-name",
     "description": "Томатна основа, моцарела, італійське м'ясо Di Parma, соус Песто, рукола",
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_79df7e77a622c6a9fd225f2d0104a3eb.jpg",
     "cost": "18",
