02155b2d47e55c2d57aa2e30ae3c3ddc51967aeb
Bilostenko <bilostenkod@gmail.com>
2023-05-19 16:50:26 +0300

    add js

--- pizza.mjs
+++ pizza.mjs
@@ -5,14 +5,16 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_f8e5d58e48dd0b15a2d15737925e4e56.jpg",
     "cost": "dolce-pere-price",
     "data-i18n": "dolce-pere",
-    "data-price": "101"
+    "data-price": "101",
+    "id": 1
   },
   {
     "pizza-name": "tendet_tomato-name",
     "description": "Вершкова основа, моцарелла, рікота, помідор, помідори черрі, пармезан",
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_d3c8726c47d80b7800a03216342be5f1.png", "cost": "tendet-tomato-price",
     "data-i18n": "tendet-tomato",
-    "data-price": "124"
+    "data-price": "124",
+    "id": 2
 
   },
   {
@@ -21,7 +23,8 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_89c9d02a6808b42b28224a4110420eb3.jpg",
     "cost": "pesto-proshuto-price",
     "data-i18n": "pesto-proshuto",
-    "data-price": "143"
+    "data-price": "143",
+    "id": 3
   },
   {
     "pizza-name": "tenerezza-name",
@@ -29,7 +32,8 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_5d530bec9f3167eb91f585e3e92cf95d.jpg",
     "cost": "tenerezza-price",
     "data-i18n": "tenerezza",
-    "data-price": "119"
+    "data-price": "119",
+    "id": 4
   },
   {
       "pizza-name": "peperoni-name",
@@ -37,7 +41,8 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_31df4042c5b3719a616a8ad40751e854.jpg",
     "cost": "peperoni-pice",
     "data-i18n": "peperoni",
-    "data-price": "460"
+    "data-price": "460",
+    "id": 5
   },
   {
      "pizza-name": "diablo-name",
@@ -45,7 +50,8 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_15f5a6329654e1ce4ea27ecef1620d0a.jpg",
     "cost": "diablo-price",
     "data-i18n": "diablo",
-    "data-price": "147"
+    "data-price": "147",
+    "id": 6
   },
   {
     "pizza-name": "salmon-name",
@@ -53,7 +59,8 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_7d20f493a26b73ed49c9226e6f705e34.jpg",
     "cost": "salmon-price",
     "data-i18n": "salmon",
-    "data-price": "123"
+    "data-price": "123",
+    "id": 7
   },
   {
     "pizza-name": "caesar-name",
@@ -61,7 +68,8 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_c22bff41004968c5603343b3642cea1d.jpg",
     "cost": "caesar-price",
     "data-i18n": "caesar",
-    "data-price": "119"
+    "data-price": "119",
+    "id": 8
   },
   {
     "pizza-name": "caprese-name",
@@ -69,7 +77,8 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_1a3f7390dfe50e2f508dd69e6f397633.JPG",
     "cost": "caprese-price",
     "data-i18n": "caprese",
-    "data-price": "140"
+    "data-price": "140",
+    "id": 9
   },
   {
     "pizza-name": "americano-name",
@@ -77,7 +86,8 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_409b140cf91887bbd07dabe8ad614549.JPG",
     "cost": "americano-price",
     "data-i18n": "americano",
-    "data-price": "109"
+    "data-price": "109",
+    "id": 10
   },
   {
     "pizza-name": "meat-2.0-name",
@@ -85,7 +95,8 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_34fd35d473dacd53e30882e98fa2e477.jpg",
     "cost": "meat-2.0-price",
     "data-i18n": "meat-2.0",
-    "data-price": "142"
+    "data-price": "142",
+    "id": 11
   },
   {
     "pizza-name": "quattro-stagioni-name",
@@ -93,7 +104,8 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_094babb1b2a781bcc51210a4f35b0217.jpg",
     "cost": "quattro-stagioni-price",
     "data-i18n": "quattro-stagioni",
-    "data-price": "110"
+    "data-price": "110",
+    "id": 12
   },
   {
     "pizza-name": "tirol-name",
@@ -101,7 +113,8 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_7e7b0a49ff059449dcd10a4bd58f25e5.jpg",
     "cost": "tirol-price",
     "data-i18n": "tirol",
-    "data-price": "111"
+    "data-price": "111",
+    "id": 13
   },
   {
     "pizza-name": "primavera-name",
@@ -109,7 +122,8 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_545107c12c1e740ac1c01e07d43e3d5f.jpg",
     "cost": "primavera-price",
     "data-i18n": "primavera",
-    "data-price": "210"
+    "data-price": "210",
+    "id": 14
   },
   {
     "pizza-name": "kaprychosa-name",
@@ -117,7 +131,8 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_651d748d82dc6ac0ff78fd0a5f6d03c7.jpg",
     "cost": "kaprychosa-price",
     "data-i18n": "kaprychosa",
-    "data-price": "190"
+    "data-price": "190",
+    "id": 15
   },
   {
     "pizza-name": "white-mashroom-name",
@@ -125,7 +140,8 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_cd5cbbf4b0de873c2631533c531a0255.jpg",
     "cost": "white-mashroom-price",
     "data-i18n": "white-mashroom",
-    "data-price": "160"
+    "data-price": "160",
+    "id": 16
   },
   {
     "pizza-name": "dandelion-name",
@@ -133,7 +149,8 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_17782bf41c8212593194888e75f6af22.jpg",
     "cost": "dandelion-price",
     "data-i18n": "dandelion",
-    "data-price": "140"
+    "data-price": "140",
+    "id": 17
   },
   {
     "pizza-name": "ukrainian-name",
@@ -141,7 +158,8 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_644c57c6479bee31db4e589488933a00.jpg",
     "cost": "ukrainian-price",
     "data-i18n": "ukrainian",
-    "data-price": "150"
+    "data-price": "150",
+    "id": 18
   },
   {
     "pizza-name": "for-buslaeva-name",
@@ -149,7 +167,8 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_79df7e77a622c6a9fd225f2d0104a3eb.jpg",
     "cost": "for-buslaeva-price",
     "data-i18n": "for-buslaeva",
-    "data-price": "160"
+    "data-price": "160",
+    "id": 19
   },
   {
     "pizza-name": "provans-name",
@@ -157,7 +176,8 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_be5ee65f0c8e2ba40abbd4cf82fe2be0.jpg",
     "cost": "provans-price",
     "data-i18n": "provans",
-    "data-price": "110"
+    "data-price": "110",
+    "id": 20
   },
   {
     "pizza-name": "vegetariana-name",
@@ -165,7 +185,8 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_6600969429ce0d35d5cc980b5dc77c17.jpg",
     "cost": "tendet-tomato-price",
     "data-i18n": "tendet-tomato",
-    "data-price": "124"
+    "data-price": "124",
+    "id": 21
   },
   {
     "pizza-name": "4-cheese-name",
@@ -173,7 +194,8 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_9cb1625db95721dcf7626949aea9d8f1.jpg",
     "cost": "tendet-tomato-price",
     "data-i18n": "tendet-tomato",
-    "data-price": "124"
+    "data-price": "124",
+    "id": 22
   },
   {
     "pizza-name": "hawaiian-name",
@@ -181,7 +203,8 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_1f8758cba36b00d27b8057b263907674.jpg",
     "cost": "hawaiian-price",
     "data-i18n": "hawaiian",
-    "data-price": "110"
+    "data-price": "110",
+    "id": 23
   },
   {
     "pizza-name": "hunter-name",
@@ -189,7 +212,8 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_dfc01c4afcbcea9b81c2fd786cd2fa66.jpg",
     "cost": "hunter-price",
     "data-i18n": "hunter",
-    "data-price": "120"
+    "data-price": "120",
+    "id": 24
   },
   {
     "pizza-name": "margaryta-name",
@@ -197,7 +221,8 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_a3a22ad43a4c3eaba0fc9e7e2fda9195.jpg",
     "cost": "margaryta-price",
     "data-i18n": "margaryta",
-    "data-price": "130"
+    "data-price": "130",
+    "id": 25
   },
   {
     "pizza-name": "meat-name",
@@ -205,7 +230,8 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_2a504a4bf3d1545f5988237ba17d2c38.jpg",
     "cost": "meat-price",
     "data-i18n": "meat",
-    "data-price": "100"
+    "data-price": "100",
+    "id": 26
   },
   {
     "name": "Шинка Та Гриби",
@@ -214,7 +240,8 @@ const pizza = [
     "image": "https://veteranopizza.com/public/uploads/site_catalog/original_373efe50da26cdbd5f79831d734d7038.jpg",
     "cost": "tendet-tomato-price",
     "data-i18n": "tendet-tomato",
-    "data-price": "124"
+    "data-price": "124",
+    "id": 27
   },
 
 ];
